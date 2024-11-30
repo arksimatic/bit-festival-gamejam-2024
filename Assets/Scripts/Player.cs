333e4a10f25e3d218042df8c5444a96b871c5082
Totokis <pawelj_janusz@wp.pl>
2024-11-30 21:17:33 +0100

    feat: add camera and input system

--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -3,11 +3,11 @@ using UnityEngine;
 public class Player : MonoBehaviour
 {
     [SerializeField] private float speed;
-    private InputSystem_Actions input;
+    private InputSystem_Actions _input;
     private void Awake()
     {
-        input = new InputSystem_Actions();
-        input.Player.Enable();
+        _input = new InputSystem_Actions();
+        _input.Player.Enable();
     }
     void Update()
     {
@@ -16,14 +16,14 @@ public class Player : MonoBehaviour
     }
     private void HandleInteraction()
     {
-        var isInteract = input.Player.Interact.IsPressed();
+        var isInteract = _input.Player.Interact.IsPressed();
 
         Debug.Log($"Is interact: {isInteract}");
     }
     private void HandleMovement()
     {
 
-        var horizontalMovement = input.Player.Move.ReadValue<Vector2>().x;
+        var horizontalMovement = _input.Player.Move.ReadValue<Vector2>().x;
 
         var movement = new Vector3(horizontalMovement, 0.0f, transform.position.z);
 
