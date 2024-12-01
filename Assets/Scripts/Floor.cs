b35046dd312ee49e726fc379da4e2cb66c4a9920
Totokis <pawelj_janusz@wp.pl>
2024-12-01 05:59:14 +0100

    fas

--- Assets/Scripts/Floor.cs
+++ Assets/Scripts/Floor.cs
@@ -6,7 +6,7 @@ using Random = UnityEngine.Random;
 public class Floor : MonoBehaviour
 {
     public List<Vector2> DoorPlaceholders;
-    public int PlayerCount;
+    private bool _isShuffleStarted;
     public List<Door> Doors => GetComponentsInChildren<Door>().ToList();
 
     private void ShuffleDoor()
@@ -24,16 +24,22 @@ public class Floor : MonoBehaviour
         var secondPos = second.transform.position;
         LeanTween.move(first.gameObject, secondPos, 1f).setEaseInOutSine();
         LeanTween.move(second.gameObject, firstPos, 1f).setEaseInOutSine();
+        _isShuffleStarted = false;
 
     }
 
 
     public void StartShuffle()
     {
-        if (Random.Range(0f, 1f) <= 0.5f)
+        if (Random.Range(0f, 1f) <= 0.25f)
         {
-            var randTime = Random.Range(1, 10);
-            Invoke(nameof(ShuffleDoor), randTime);
+            if (!_isShuffleStarted)
+            {
+                var randTime = Random.Range(1, 10);
+                Invoke(nameof(ShuffleDoor), randTime);
+                _isShuffleStarted = true;
+            }
+
         }
     }
 }
